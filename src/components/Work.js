a73229ae3303c942ad6b500d58bfbb5204a8c545
Brian Fitzsimmons <bfitzsimmons703@gmail.com>
2022-04-28 16:49:27 -0700

    Updated work copy for playlive bullet

--- src/components/Work.js
+++ src/components/Work.js
@@ -11,7 +11,7 @@ function Work() {
                 <p>Software Engineer, Sept 2017 - April 2022</p>
                 <li>Leveraged knowledge in JavaScript, PHP, SQL, and AWS to develop full-stack features across Sporcle.com&apos;s desktop and mobile sites, helping it scale to 1M+ gameplays per day.</li>
                 <li>Project owner for Sporcle&apos;s recommendation engine. Used Elasticsearch, Redis, and collaborative filtering ML to serve personalized content to 550,000+ users.</li>
-                <li>Collaborated with Sporcle CTO and customers to develop a real-time multiplayer scoring app for live trivia that now generates over $50,000 per week.</li>
+                <li>Collaborated with Sporcle CTO and customers to develop a real-time multiplayer scoring app for live trivia that now serves hundreds of shows per week.</li>
                 <li>Learned Git, React Native, and mobile app development to create Sporcle Word Ladder, a mobile trivia game available on iOS and Android with a 4.5 star rating.</li>
                 <li>Learned Swift to build the majority of Sporcle&apos;s revamped iOS app, including in-app purchases, social authentication, web-view rendering, and backend infrastructure.</li>
                 <li>Used Tableau and SQL (MySQL, Redshift) to produce graphs of Sporcle&apos;s KPIs and investigate user behavior, bringing a data-centric/evidence-based approach to product development.</li>
